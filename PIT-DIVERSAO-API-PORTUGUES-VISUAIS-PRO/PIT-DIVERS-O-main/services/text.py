0fdef2a4b0615e971efb8aaebbe2f981ac0cdc0a
poze05698-collab <poze05698@gmail.com>
2026-09-17 14:01:58 -0300

    Delete PIT-DIVERSAO-API-PORTUGUES-VISUAIS-PRO/PIT-DIVERS-O-main directory

--- PIT-DIVERSAO-API-PORTUGUES-VISUAIS-PRO/PIT-DIVERS-O-main/services/text.py
+++
@@ -1,3 +0,0 @@
-import re,unicodedata
-def norm(s):
- s=(s or '').strip().casefold(); s=unicodedata.normalize('NFKD',s); s=''.join(x for x in s if not unicodedata.combining(x)); return re.sub(r'\s+',' ',s)
