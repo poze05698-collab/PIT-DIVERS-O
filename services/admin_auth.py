1ca9ff18c513ba3774576f5acc62c5a43c5f11fe
poze05698-collab <poze05698@gmail.com>
2026-09-16 22:04:31 -0300

    Delete services directory

--- services/admin_auth.py
+++
@@ -1,4 +0,0 @@
-from config import ADMIN_IDS
-
-def is_global_admin(user_id: int) -> bool:
-    return int(user_id) in ADMIN_IDS
--- services/permissions.py
+++
@@ -1,5 +0,0 @@
-from aiogram import Bot
-from aiogram.enums import ChatMemberStatus
-async def admin(bot:Bot,chat:int,user:int):
- try:return (await bot.get_chat_member(chat,user)).status in {ChatMemberStatus.ADMINISTRATOR,ChatMemberStatus.CREATOR}
- except:return False
